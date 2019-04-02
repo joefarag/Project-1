8abc80b8bfd5f9f4a4b68f688d454be1a1d6ffcf
Joseph Farag <joseph.farag@uon.edu.au>
2019-04-02 23:41:35 +0000

    Added start of encryption of rotation

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main() {
-
+    
+  char str[] = "abcdefghijklmnopqrstuvwxyz", c; 
+  int i, x; 
+  printf("Encryption message: \n");
+  scanf("%d", &x);
+  printf("Key: \n");
+  
+f
   return 0;
 }
